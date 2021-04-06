43b0836800763e81582d5ff0c643a89a2dceb338
othwsav <54482114+othwsav@users.noreply.github.com>
2021-04-06 21:15:37 +0100

    new branch finished where you can add change between cities whithout reloading and with changing url

--- src/App.js
+++ src/App.js
@@ -1,68 +1,38 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import {BrowserRouter, Route, Link} from 'react-router-dom'
+
 import './App.css';
 import Form from './components/Form'
 import Weather from './components/Weather'
-import Load from './components/Load'
+// import Load from './components/Load'
 
 
-class App extends Component {
+class App extends Component{
+
 	state = {
-		wheatherVals : [
-			'', '', '', '', '', '', '', ''
-		],
-		wheatherNames : [
-			'city', 'country', 'description', 'temperature', 'humidity', 'pressure', 'wind degree', 'wind speed'
-		],
-		iconUrl: '',
-		error: true,
-		loading: false
+		pathname: ""
 	}
-
-
-
-	getWeather = async (e) => {
+	
+	getWeather = async(e) => {
 		e.preventDefault()
 		await this.setState({
-			loading: true
+			pathname: "/" + e.target.elements.city.value
 		})
+		// window.location.pathname = "/" + e.target.elements.city.value
+		await document.getElementById("togoLink").click()
+    }
 
-		let APIKEY, city, API, data
-		try {
-			APIKEY = "66941712dc19e2561d1da16dad62bc33"
-			city = e.target.elements.city.value
-			API = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=metric`)
-			data = await API.json();
-			this.setState({
-				wheatherVals:[
-					data.name, data.sys.country, data.weather[0].description, data.main.temp + "°C", data.main.humidity + "%", data.main.pressure + "hPa", data.wind.deg + "°", data.wind.speed + "Km/h"
-				],
-				iconUrl: process.env.PUBLIC_URL + `images/GWI/${data.weather[0].icon}.svg`,//`http://openweathermap.org/img/w/${data.weather[0].icon}.png`,
-				error: false
-			})
-			
-			await this.setState({
-				loading: false
-			})
-			
-		} catch (error) {
-			this.setState({
-				error: (data)? data.message : "Connection problem, Check out your Internet!"
-			})
 
-			await this.setState({
-				loading: false
-			})
-		}
-	
-	}
 
-	render() {
+	render(){
 		return (
-			<div className="App">
-				<Form get={this.getWeather}/>
-				{(this.state.loading)? <Load/> : <Weather wev={this.state.wheatherVals} wen={this.state.wheatherNames} icon={this.state.iconUrl} error={this.state.error}/>
-				}
-			</div>
+			<BrowserRouter>
+				<div className="App">
+						<Form get={this.getWeather}/>
+						<Link id="togoLink" to={this.state.pathname} />
+						<Route path="/:city" component={Weather} />
+				</div>
+			</BrowserRouter>
 		)
 	}
 }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,44 +1,107 @@
-import React from 'react'
+// import { render } from '@testing-library/react'
+import React, {Component} from 'react'
+import {withRouter} from 'react-router-dom'
 
-const Weather = props => {
+import Load from './Load'
 
-
-    const {wev,wen,icon,error} = props
+class Weather extends Component {
     
-    let wElem, wElems, iconElem, errorElem
-    
-    if(error === false){
-        wElems = wen.map((e,i) => {
-            return  <div key={i} className="data_element">
-                        <span>
-                            <img src={process.env.PUBLIC_URL + `/images/${i}.svg`} alt={e + ' wheather'} />
-                        </span>
-                        <span>{e}:</span>
-                        <span>{wev[i]}</span>
-                    </div>
+
+    state = {
+		wheatherVals : [
+			'', '', '', '', '', '', '', ''
+		],
+		wheatherNames : [
+			'city', 'country', 'description', 'temperature', 'humidity', 'pressure', 'wind degree', 'wind speed'
+		],
+		iconUrl: '',
+		error: true,
+		loading: false
+	}
+
+    doSearch = async () => {
+        // await console.log(this.state)
+        await this.setState({
+            loading: true
         })
 
-        wElem = <div className="elems">{wElems}</div>
+        let APIKEY, city, API, data
+        try {
+            APIKEY = "66941712dc19e2561d1da16dad62bc33"
+            city = this.props.match.params.city
+            API = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=metric`)
+            data = await API.json();
+            this.setState({
+                wheatherVals:[
+                    data.name, data.sys.country, data.weather[0].description, data.main.temp + "°C", data.main.humidity + "%", data.main.pressure + "hPa", data.wind.deg + "°", data.wind.speed + "Km/h"
+                ],
+                iconUrl: process.env.PUBLIC_URL + `images/GWI/${data.weather[0].icon}.svg`,//`http://openweathermap.org/img/w/${data.weather[0].icon}.png`,
+                error: false
+            })
+            
+            await this.setState({
+                loading: false
+            })
+            
+        } catch (error) {
+            this.setState({
+                error: (data)? data.message : "Connection problem, Check out your Internet!"
+            })
 
-        iconElem =  <div className="icon">
+            await this.setState({
+                loading: false
+            })
+        }
+        // await console.log(this.state)
+        this.x = true
+    }
+
+    showW = () => {
+
+        const {wheatherVals,wheatherNames,iconUrl,error} = this.state
+        
+        if(error === false){
+            let wElems = wheatherNames.map((e,i) => {
+                return  <div key={i} className="data_element">
+                            <span>
+                                <img src={process.env.PUBLIC_URL + `/images/${i}.svg`} alt={e + ' wheather'} />
+                            </span>
+                            <span>{e}:</span>
+                            <span>{wheatherVals[i]}</span>
+                        </div>
+            })
+
+            return(
+                <>
+                    <div className="icon">
                         <span>general weather:</span>
                         <div>
-                            <img src={icon} alt="weather icon"/>
+                            <img src={iconUrl} alt="weather icon"/>
                         </div>
                     </div>
-    }else{
-        if(typeof error === "string")
-            errorElem = <div className="error">{error}</div>
-    }
-    
+                    <div className="elems">{wElems}</div>
+                </>
+            )
 
-    return (
-        <div>
-            {iconElem}
-            {wElem}
-            {errorElem}
-        </div>
-    )
+        }else{
+            if(typeof error === "string")
+                return (<div className="error">{error}</div>)
+        }
+    }
+
+    x = true
+    render(){
+        if(this.x){
+            this.x = false
+            this.doSearch()
+        }
+        return (
+            <div>
+                {(this.state.loading)? <Load/> :  this.showW()}
+            </div>
+        )
+    }
+
 }
 
-export default Weather
+export default withRouter(Weather);
